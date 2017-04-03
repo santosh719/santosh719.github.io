c8abb4c138b15e31434a5fd96d363f7a93ad045e
Santosh Bidve <santoshbidave2@gmail.com>
2017-04-03 14:38:57 -0700

    email functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -296,28 +296,28 @@ jQuery(function ($) {
         var $this = $(this);
         $this.before( '<div class="alert alert-success">'+data.message+'<p><strong>Thank you for your message!</strong></p></div>' );
 
-        e.preventDefault();
-
-        var $action = $(this).prop('action');
-        var $data = $(this).serialize();
-
-
-        $this.prevAll('.alert').remove();
-
-        $.post( $action, $data, function( data ) {
-
-            if( data.response=='error' ){
-
-                $this.before( '<div class="alert alert-danger">'+data.message+'</div>' );
-            }
-
-            if( data.response=='success' ){
-
-                $this.before( '<div class="alert alert-success">'+data.message+'</div>' );
-                $this.find('input, textarea').val('');
-            }
-
-        }, "json");
+        // e.preventDefault();
+        //
+        // var $action = $(this).prop('action');
+        // var $data = $(this).serialize();
+        //
+        //
+        // $this.prevAll('.alert').remove();
+        //
+        // $.post( $action, $data, function( data ) {
+        //
+        //     if( data.response=='error' ){
+        //
+        //         $this.before( '<div class="alert alert-danger">'+data.message+'</div>' );
+        //     }
+        //
+        //     if( data.response=='success' ){
+        //
+        //         $this.before( '<div class="alert alert-success">'+data.message+'</div>' );
+        //         $this.find('input, textarea').val('');
+        //     }
+        //
+        // }, "json");
 
     });
 
